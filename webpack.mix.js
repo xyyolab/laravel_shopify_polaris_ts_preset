571e0221b69ebca2a5d6c8daae388bb1e6e297ed
XING YAHAO <keidarcy.1015@gmail.com>
2020-08-21 18:55:47 +0900

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,6 @@ let mix = require('laravel-mix')
 
 require('laravel-mix-react-typescript-extension')
 
-mix
-  .reactTypeScript('resources/ts/app.tsx', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css')
+mix.reactTypeScript('resources/ts/app.tsx', 'public/js')
 
 mix.disableSuccessNotifications().version()
